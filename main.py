89772deb245755a0057263f40b9055bd84e486df
Wenchao-Gao <wenchao_g@yahoo.co.jp>
2020-03-05 18:05:14 +0900

    escape

--- main.py
+++ main.py
@@ -43,6 +43,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    txt = event.message.text
+    txt = txt + "ですね"
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
