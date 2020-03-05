dfe1ab1fe2b163d2717ced3689318f7f2c6e50c9
Wenchao-Gao <wenchao_g@yahoo.co.jp>
2020-03-05 18:09:56 +0900

    escape

--- main.py
+++ main.py
@@ -45,9 +45,10 @@ def callback():
 def handle_message(event):
     txt = event.message.text
     txt = txt + "ですね"
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=txt))
 
 if __name__ == "__main__":
 #    app.run()
